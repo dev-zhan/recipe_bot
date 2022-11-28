083e1d1076190bd45588407002e55babad1688e3
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-28 20:55:47 +0600

    added queries for sql!

---
+++ queries.sql
@@ -0,0 +1,21 @@
+REATE DATABASE recipe_bot_db;
+
+USE recipe_bot_db;
+
+CREATE TABLE users(
+                      id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                      name VARCHAR(255),
+                      telegram_id INT,
+                      first_name VARCHAR(255),
+                      last_name VARCHAR(255),
+                      chat_id INT,
+                      created_at DATETIME default CURRENT_TIMESTAMP,
+                      updated_at DATETIME,
+                      deleted_at DATETIME
+);
+
+CREATE TABLE mails(
+                      id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                      letter VARCHAR(255),
+                      ReceivedDate DATETIME default CURRENT_TIMESTAMP
+);
