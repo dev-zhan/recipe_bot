d7b20a55c5890056bf14d341a77a326fb685edef
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-28 20:14:40 +0600

    added function to init bot

---
+++ cmd/bot.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"gopkg.in/telebot.v3"
+	"log"
+	"time"
+)
+
+func InitBot(token string) *telebot.Bot {
+	pref := telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := telebot.NewBot(pref)
+
+	if err != nil {
+		log.Fatalf("Ошибка при инициализации бота %v", err)
+	}
+
+	return b
+}
