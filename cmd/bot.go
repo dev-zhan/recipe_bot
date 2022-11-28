78788666e6178998d5838ce26a457e8b41cb179e
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-28 20:16:03 +0600

    Created model of bot and StartHandler!

--- cmd/bot.go
+++ cmd/bot.go
@@ -3,8 +3,40 @@ package cmd
 import (
 	"gopkg.in/telebot.v3"
 	"log"
+	"recipe_bot/internal/models"
+	"recipe_bot/internal/repository"
 	"time"
 )
+
+type UpgradeBot struct {
+	Bot   *telebot.Bot
+	Users *repository.UserModel
+}
+
+func (bot *UpgradeBot) StartHandler(ctx telebot.Context) error {
+	newUser := models.User{
+		Name:       ctx.Sender().Username,
+		TelegramId: ctx.Chat().ID,
+		FirstName:  ctx.Sender().FirstName,
+		LastName:   ctx.Sender().LastName,
+		ChatId:     ctx.Chat().ID,
+	}
+
+	existUser, err := bot.Users.FindOne(ctx.Chat().ID)
+
+	if err != nil {
+		log.Printf("Такой пользователь уже существует %v", err)
+	}
+
+	if existUser == nil {
+		err := bot.Users.Create(newUser)
+		if err != nil {
+			log.Printf("Ошибка создания пользователя %v", err)
+		}
+	}
+
+	return ctx.Send("Hello, " + ctx.Sender().FirstName + "\nDon't know what to cook today? Reciper Teller is here to help you. \n\n 1. Get random recipe with /random \n 2. Get recipe by name /name {enter here recipe name}\n 3. Get random recipe with one ingredient /ingredient {enter ingredient name}")
+}
 
 func InitBot(token string) *telebot.Bot {
 	pref := telebot.Settings{
